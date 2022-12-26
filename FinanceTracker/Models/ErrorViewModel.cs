c29554c7f865135a4ee0fce00abcb9a2da98611d
GookProg <kalter534226@yandex.ru>
2022-12-26 17:36:28 +0300

    Add Category and Transaction models

--- FinanceTracker/Models/ErrorViewModel.cs
+++ FinanceTracker/Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace FinanceTracker.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace FinanceTracker.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
