2ebe8b0b10f9a89b8caf122cda36e7a2c781ea67
GookProg <kalter534226@yandex.ru>
2022-12-28 12:57:42 +0300

    Update Category controller

--- FinanceTracker/Controllers/CategoryController.cs
+++ FinanceTracker/Controllers/CategoryController.cs
@@ -14,16 +14,10 @@ public class CategoryController : Controller
 {
     private readonly ApplicationDbContext _context;
 
-    public CategoryController(ApplicationDbContext context)
-    {
-        _context = context;
-    }
+    public CategoryController(ApplicationDbContext context) => _context = context;
 
     // GET: Category
-    public async Task<IActionResult> Index()
-    {
-          return View(await _context.Category.ToListAsync());
-    }
+    public async Task<IActionResult> Index() => View(await _context.Category.ToListAsync());
 
     // GET: Category/Details/5
     public async Task<IActionResult> Details(int? id)
@@ -44,15 +38,20 @@ public class CategoryController : Controller
     }
 
     // GET: Category/Create
-    public IActionResult Create()
+    public IActionResult Upsert(int id = 0)
     {
-        return View(new Category());
+        if (id == 0)
+        {
+            return View(new Category());
+        }
+
+        return View(_context.Category.Find(id));
     }
 
     // POST: Category/Create
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("CategoryId,Title,Icon,Type")] Category category)
+    public async Task<IActionResult> Upsert([Bind("CategoryId,Title,Icon,Type")] Category category)
     {
         if (ModelState.IsValid)
         {
@@ -60,55 +59,7 @@ public class CategoryController : Controller
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
-        return View(category);
-    }
 
-    // GET: Category/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null || _context.Category == null)
-        {
-            return NotFound();
-        }
-
-        var category = await _context.Category.FindAsync(id);
-        if (category == null)
-        {
-            return NotFound();
-        }
-        return View(category);
-    }
-
-    // POST: Category/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [Bind("CategoryId,Title,Icon,Type")] Category category)
-    {
-        if (id != category.CategoryId)
-        {
-            return NotFound();
-        }
-
-        if (ModelState.IsValid)
-        {
-            try
-            {
-                _context.Update(category);
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CategoryExists(category.CategoryId))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-            return RedirectToAction(nameof(Index));
-        }
         return View(category);
     }
 
@@ -149,8 +100,5 @@ public class CategoryController : Controller
         return RedirectToAction(nameof(Index));
     }
 
-    private bool CategoryExists(int id)
-    {
-      return _context.Category.Any(e => e.CategoryId == id);
-    }
+    private bool CategoryExists(int id) => _context.Category.Any(e => e.CategoryId == id);
 }
