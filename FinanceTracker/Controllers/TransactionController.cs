07987f7a0e11187045b3277b25ce291176c85750
GookProg <kalter534226@yandex.ru>
2023-01-05 14:40:05 +0300

    Refactor Transaction controller

--- FinanceTracker/Controllers/TransactionController.cs
+++ FinanceTracker/Controllers/TransactionController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FinanceTracker.Data;
 using FinanceTracker.Models;
@@ -14,10 +9,7 @@ public class TransactionController : Controller
 {
     private readonly ApplicationDbContext _context;
 
-    public TransactionController(ApplicationDbContext context)
-    {
-        _context = context;
-    }
+    public TransactionController(ApplicationDbContext context) => _context = context;
 
     // GET: Transaction
     public async Task<IActionResult> Index()
