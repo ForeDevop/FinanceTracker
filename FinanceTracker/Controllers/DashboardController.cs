b08f8f5d3fa9376568e128f617c52f715db8a4fe
GookProg <kalter534226@yandex.ru>
2023-01-05 15:33:41 +0300

    Update Dashboard controller

--- FinanceTracker/Controllers/DashboardController.cs
+++ FinanceTracker/Controllers/DashboardController.cs
@@ -1,11 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FinanceTracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanceTracker.Controllers;
 
 public class DashboardController : Controller
 {
-    public IActionResult Index()
+    private readonly ApplicationDbContext _context;
+
+    public DashboardController(ApplicationDbContext context) => _context = context;
+
+    public async Task<IActionResult> Index()
     {
+        const int DaysOfWeek = 7;
+        var startDate = DateTime.Today.AddDays(-DaysOfWeek);
+        var endDate = DateTime.Today;
+
+        var selectedTransactions = await _context.Transactions
+            .Include(t => t.Category)
+            .Where(c => c.Date > startDate && c.Date <= endDate)
+            .ToListAsync();
+
+        var totalIncome = selectedTransactions
+            .Where(t => t.Category.Type == "Income")
+            .Sum(i => i.Amount);
+        ViewBag.TotalIncome = totalIncome.ToString("C0");
+
+        var totalExpense = selectedTransactions
+            .Where(t => t.Category.Type == "Expense")
+            .Sum(e => e.Amount);
+        ViewBag.TotalExpense = totalExpense.ToString("C0");
+
+        var balance  = totalIncome - totalExpense;
+        ViewBag.Balance = balance.ToString();
+
         return View();
     }
 }
