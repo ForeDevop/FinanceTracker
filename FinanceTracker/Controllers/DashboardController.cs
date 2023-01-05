64548f360068e35cd48d827b11a7f1e7ba4bbcb3
GookProg <kalter534226@yandex.ru>
2023-01-05 14:45:26 +0300

    Add Dasboard controller and view

---
+++ FinanceTracker/Controllers/DashboardController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceTracker.Controllers;
+
+public class DashboardController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
